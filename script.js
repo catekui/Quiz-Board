30de51401360ff6442f85a64171fccf7ad9061bc
catekui <catherine.wanjiku@student.moringaschool.com>
2022-02-06 19:04:19 +0300

    feature:added conditional statements

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function check () {
     var a=0
-    var q1=document.Quiz.question1.value;
-    var q2=document.Quiz.question2.value;
-    var q3=document.Quiz.question3.value;
-    var q4=document.Quiz.question4.value;
-    var q5=document.Quiz.question5.value;
-
-
+    var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
 
     if (q1=="script") {a++};
     if (q2=="true") {a++}
@@ -14,8 +12,23 @@ function check () {
     if (q4=="Scripting") {a++};
     if (q5=="italics()") {a++};
 
-    document.write(a);
+    quiz.style.display="none";
+    var final=a*20;
+    document.write("Your Score is "+final+" %")
 
+    if (final>=80) {
+        document.write(" Excellent") 
+    }
+    else if (final>=60 && final<80) {
+        document.write(" You have scored fairly")
+    }
+    else if (final<50) {
+        document.write(" You have scored poorly. Please repeat the test")
+    }
+    
+    
+
+   
 
 
 
