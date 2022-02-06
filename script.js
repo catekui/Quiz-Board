541a8411319b2944b5a585af427382aa04286d59
catekui <catherine.wanjiku@student.moringaschool.com>
2022-02-06 17:20:45 +0300

    chore:added an id by the name results

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function check () {
     var a=0
-    var question1=document.Quiz.question1.value;
-    var question2=document.Quiz.question2.value;
-    var question3=document.Quiz.question3.value;
-    var question4=document.Quiz.question4.value;
-    var question5=document.Quiz.question5.value;
+    var q1=document.Quiz.question1.value;
+    var q2=document.Quiz.question2.value;
+    var q3=document.Quiz.question3.value;
+    var q4=document.Quiz.question4.value;
+    var q5=document.Quiz.question5.value;
 
 
 
-    if (question1=="script") {a++};
-    if (question2=="true") {a++}
-    if (question3=="undefined") {a++};
-    if (question4=="Scripting") {a++};
-    if (question5=="italics()") {a++};
+    if (q1=="script") {a++};
+    if (q2=="true") {a++}
+    if (q3=="undefined") {a++};
+    if (q4=="Scripting") {a++};
+    if (q5=="italics()") {a++};
 
     document.write(a);
 
